4157858b3b683e80526c061fb0e938e958b2a970
Арсений <arseny32.32@yandex.ru>
2024-04-06 14:38:03 +0300

    06.04.24

---
+++ ege02/257.py
@@ -0,0 +1,6 @@
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                f =  ((not x or y and w ) and ( not z or x or y )) != w
+                print(x, y, z, w, "!", int(f))
---
+++ ege02/258.py
@@ -0,0 +1,6 @@
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                f =  not ( not ( not ( not ( not w ) or ( not y ) ) or ( not z ))  or x )
+                print(x, y, z, w, "!", int(f))
