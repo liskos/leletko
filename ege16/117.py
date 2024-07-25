f8f59560355ee8c0669006b1aadd7a2f513a9925
liskos <liskosnik2011@yandex.ru>
2024-07-25 21:05:19 +0300

    117 задание

--- ege16/117.py
+++ ege16/117.py
@@ -1,19 +1,20 @@
 import functools
-@functools.lru_cache(None)
-def f(n):
+@functools.lru_cache(1000000)
+def f(n, a):
     if n == 0:
         return 1
-    if n > 0 and n % 2 != 0:
-        return 1 + f(n - 1)
-    if n == 4:
-        return 4
-    else:
-        return  f(n // 2)
+    if a == 3:  # если глубина погружения 4, то дальше не считай, нет смысла, уже больше 3
+        return 1
+    if n % 2 != 0:
+        return 1 + f(n - 1, a+1) # если прибавляет 1 к значению, то а увеличиваем на 1
+    return f(n // 2, a)   # тут функция не изменяется, значит и а мы не увеличиваем
 
 
 
 a = 0
-for i in range(1, 500000000):
-    if f(i) == 3:
+for i in range(1, 500000000+1):
+    if f(i, 0) == 3:
         a = a + 1
+    if i % 1000000 == 0:
+        print(i, a)
 print(a)
