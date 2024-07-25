570bb337d2f55c9b0de384c4abb18d7a4ae0ec7f
Арсений <arseny32.32@yandex.ru>
2024-07-25 20:45:14 +0300

    .

---
+++ ege16/117.py
@@ -0,0 +1,19 @@
+import functools
+@functools.lru_cache(None)
+def f(n):
+    if n == 0:
+        return 1
+    if n > 0 and n % 2 != 0:
+        return 1 + f(n - 1)
+    if n == 4:
+        return 4
+    else:
+        return  f(n // 2)
+
+
+
+a = 0
+for i in range(1, 500000000):
+    if f(i) == 3:
+        a = a + 1
+print(a)
