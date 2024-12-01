1b5cc13ef8e2caddb7dc30e7bcd05d4842a1c8e1
kos <liskosnik@yandex.ru>
2024-12-01 12:07:55 +0300

    01/12/2024

--- варианты 2025/колчев/24.py
+++ варианты 2025/колчев/24.py
@@ -1,27 +1,21 @@
 import sys
 sys.stdin=open("24.txt")
 s = input()
-t = s[0]
-su = 0
-r = []
+t = ""
 m = 0
-for i in range(1, len(s)):
-    if t[0] in "+*" or (t[-1] in "+*" and s[i] in "+*"):
-        t = s[i]
-        su = 0
-    elif t[-1] == "+" and s[i] in "789":
-        t += s[i]
-        su += int(s[i])
-    elif t[-1] in "789" and s[i] in "+":
-        t += s[i]
-    elif t[-1] in "789" and s[i] in "789":
-        dp = 0
-        while i <= len(s) and t[-1] in "789" and s[i] in "789":
-            t += s[i]
-            dp += int(s[i])
-            i = i + 1
-        su += dp
-        if su > m:
-            m = su
-
+for i in s:
+    if (t == "") and (i in "*+"):
+        t = ""
+    elif (t == "") and (i in "789"):
+        t = i
+    elif (t[-1] == "+") and (i in "789"):
+        t += i
+    elif (t[-1] == "+") and (i in "+*"):
+        t = ""
+    elif (t[-1] in "789") and (i in "+789"):
+        t += i
+    elif (t[-1] in "789") and (i in "*"):
+        t = ""
+    if t and t[-1] != "+":
+        m = max(m, eval(t))
 print(m)
